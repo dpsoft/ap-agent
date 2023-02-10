516ede99d4ac14f5fed9037555d5a99d32480e38
Diego <diegolparra@gmail.com>
2023-02-10 12:27:54 -0300

    main

--- agent/src/main/java/io/github/dpsoft/ap/Agent.java
+++ agent/src/main/java/io/github/dpsoft/ap/Agent.java
@@ -1,5 +1,7 @@
 package io.github.dpsoft.ap;
 
+import io.github.dpsoft.ap.context.api.Context;
+import io.github.dpsoft.ap.context.api.ContextHandler;
 import io.github.dpsoft.ap.handler.AsyncProfilerHandler;
 import io.github.dpsoft.ap.instrumentation.ContextInstrumenter;
 import io.github.dpsoft.ap.util.Banner;
@@ -7,6 +9,7 @@ import io.github.dpsoft.ap.util.Runner;
 import io.github.dpsoft.ap.util.Server;
 
 import java.lang.instrument.Instrumentation;
+import java.util.Map;
 
 public final class Agent {
     public static void premain(String args, Instrumentation instrumentation)  {
@@ -22,4 +25,24 @@ public final class Agent {
             });
         });
     }
+
+//    public static void main(String[] args) {
+//        premain(null, ByteBuddyAgent.install());
+//
+//        var contextStorage = new ContextHandler();
+//
+//        var xx = contextStorage.runWithContext(new Context(1, Map.of("a", "b")), () -> {
+//            var currentContext = contextStorage.currentContext();
+//            return currentContext;
+//        });
+//
+////       var xxxx =  contextStorage.storeContext(new Context(2, Map.of("c", "d")));
+//
+//        System.out.println(xx.contextId);
+//        System.out.println(xx.tags);
+//
+//        System.out.println(contextStorage.currentContext().contextId);
+//
+//        while (true) {}
+//    }
 }
