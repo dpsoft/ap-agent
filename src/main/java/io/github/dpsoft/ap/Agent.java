3c7790942f79a95b16077837652a0a47b833ae2b
Diego <diegolparra@gmail.com>
2023-05-02 17:48:22 -0300

    clean up

--- src/main/java/io/github/dpsoft/ap/Agent.java
+++ src/main/java/io/github/dpsoft/ap/Agent.java
@@ -19,41 +19,4 @@ public final class Agent {
             });
         });
     }
-
-    public static void main(String[] args) {
-        premain("", null);
-        while (true) {
-            allocArray();
-            primes();
-        }
-
-    }
-
-    public static void allocArray() {
-        int[] testarray = new int[1000];
-
-        for (int i = 0; i < testarray.length; i++) {
-            testarray[i] = i;
-        }
-    }
-
-    public static void primes() {
-        while (true) {
-            int n = 1000000;
-            int[] primes = new int[n];
-            int count = 0;
-            for (int i = 2; i < n; i++) {
-                boolean isPrime = true;
-                for (int j = 0; j < count; j++) {
-                    if (i % primes[j] == 0) {
-                        isPrime = false;
-                        break;
-                    }
-                }
-                if (isPrime) {
-                    primes[count++] = i;
-                }
-            }
-        }
-    }
 }
